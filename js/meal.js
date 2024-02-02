be0b1bceba052b8ab16cde8aa4ef69fa0517cad1
ShovanTheMan <shovan2192@gmail.com>
2024-02-02 23:27:58 +0600

    modal added

--- js/meal.js
+++ js/meal.js
@@ -19,6 +19,13 @@ const displayMeals = meals =>{
        <div class="card-body">
          <h5 class="card-title">${meal.strMeal}</h5>
          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+
+     <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+       Details
+    </button>
+
+
        </div>
        </div>
        `
@@ -30,4 +37,23 @@ const searchMeals = () =>{
     console.log(searchText);
     loadMeals(searchText);
 }
-loadMeals('rice');
+const loadMealDetail = idMeal =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetailsMeal(data.meals[0]))
+    
+}
+
+const displayDetailsMeal = meal =>{
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealDetails = document.getElementById('mealDetailsBody');
+    mealDetails.innerHTML = `
+    <img class="img-fluid" src="${meal.strMealThumb}">
+    
+    `
+}
+
+
+
+loadMeals('fish');
